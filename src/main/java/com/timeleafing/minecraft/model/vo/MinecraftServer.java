483871fcf643fc7bc82dec250928804dd776daf8
TimeLeafing <1780884916@qq.com>
2026-02-04 19:16:14 +0800

    style: 修改代码格式。

--- src/main/java/com/timeleafing/minecraft/model/vo/MinecraftServer.java
+++ src/main/java/com/timeleafing/minecraft/model/vo/MinecraftServer.java
@@ -2,6 +2,7 @@ package com.timeleafing.minecraft.model.vo;
 
 import com.timeleafing.minecraft.common.enumeration.ServerStatus;
 
-public record MinecraftServer(String serverId, ServerStatus status) {
-
-}
+public record MinecraftServer(
+        String serverId,
+        ServerStatus status
+) {}
