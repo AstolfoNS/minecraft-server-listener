7fb66b575f8b6f98e7d4b8d7751edfe2bc83a287
TimeLeafing <1780884916@qq.com>
2026-02-04 17:54:23 +0800

    fix: 修复了已知的错误。

--- src/main/java/com/timeleafing/minecraft/exception/BizException.java
+++ src/main/java/com/timeleafing/minecraft/exception/BizException.java
@@ -7,7 +7,20 @@ public class BizException extends RuntimeException {
     @Serial
     private static final long serialVersionUID = 1L;
 
+
+    public BizException() {
+        super();
+    }
+
     public BizException(String message) {
         super(message);
+    }
+
+    public BizException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BizException(Throwable cause) {
+        super(cause);
     }
 }
