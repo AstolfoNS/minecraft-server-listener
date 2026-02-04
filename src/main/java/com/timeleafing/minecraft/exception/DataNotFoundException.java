87dc758c165bc047c81b10546e27768e5a5eaf57
TimeLeafing <1780884916@qq.com>
2026-02-04 17:54:27 +0800

    fix: 修复了已知的错误。

--- src/main/java/com/timeleafing/minecraft/exception/DataNotFoundException.java
+++ src/main/java/com/timeleafing/minecraft/exception/DataNotFoundException.java
@@ -7,7 +7,20 @@ public class DataNotFoundException extends RuntimeException {
     @Serial
     private static final long serialVersionUID = 1L;
 
+
+    public DataNotFoundException() {
+        super();
+    }
+
     public DataNotFoundException(String message) {
         super(message);
+    }
+
+    public DataNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DataNotFoundException(Throwable cause) {
+        super(cause);
     }
 }
--- src/main/java/com/timeleafing/minecraft/exception/RateLimitedException.java
+++ src/main/java/com/timeleafing/minecraft/exception/RateLimitedException.java
@@ -7,7 +7,20 @@ public class RateLimitedException extends RuntimeException {
     @Serial
     private static final long serialVersionUID = 1L;
 
+
+    public RateLimitedException() {
+        super();
+    }
+
     public RateLimitedException(String message) {
         super(message);
+    }
+
+    public RateLimitedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public RateLimitedException(Throwable cause) {
+        super(cause);
     }
 }
